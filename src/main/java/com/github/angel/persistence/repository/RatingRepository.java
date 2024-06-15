460d553dc9f2e3136a8e5101df3ad8400aa713d9
Ángel Aguero <angelagueror23@gmail.com>
2024-06-14 22:55:51 -0400

    feat: Implementación de la capa de repository para Movie, User y Rating
    - Creación del repositorio MovieRepository para la gestión de entidades Movie.
    - Creación del repositorio UserRepository para la gestión de entidades User.
    - Creación del repositorio RatingRepository para la gestión de entidades Rating.
    - Se añadieron métodos CRUD básicos para cada repositorio.
    - Se configuraron las relaciones necesarias entre las entidades.

--- src/main/java/com/github/angel/persistence/repository/RatingRepository.java
+++ src/main/java/com/github/angel/persistence/repository/RatingRepository.java
@@ -3,7 +3,17 @@ package com.github.angel.persistence.repository;
 import com.github.angel.persistence.entity.Rating;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RatingRepository extends JpaRepository<@Valid Rating, @Min(1) Long> {
+    List<Rating> findByMovieId(@Min(1) Long movieId);
+    @Query("SELECT r FROM Rating r JOIN r.user u WHERE u.username = :username")
+    List<Rating> findByUsername(@NotNull @Param("username") String  username);
+
+
 }
--- src/main/java/com/github/angel/persistence/repository/UserRepository.java
+++ src/main/java/com/github/angel/persistence/repository/UserRepository.java
@@ -3,7 +3,17 @@ package com.github.angel.persistence.repository;
 import com.github.angel.persistence.entity.User;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<@Valid User, @Min(1) Long> {
+    List<User> findByNameContaining(@NotNull String name);
+    Optional<User> findByUsername(@NotNull String username);
+    @Modifying
+    void deleteByUsername(@NotNull String username);
+
 }
